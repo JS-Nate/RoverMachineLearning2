0371b820acadcc722e6b0210d3cc9252f9b3766c
Nathanael 100783830 <nathanael.selvaraj@ontariotechu.net>
2024-11-15 14:01:34 -0500

    training model

--- planets2-1/trainmodel.py
+++ planets2-1/trainmodel.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
 model = YOLO("yolo11m.pt")
-model.train(data="data.yaml", imgsz=640, batch=8, epochs=100, workers=1, device="gpu")
+model.train(data="data.yaml", imgsz=640, batch=8, epochs=75, workers=1, device="cpu")
